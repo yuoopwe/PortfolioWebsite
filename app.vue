eef76aaa7129d8ecc6e0d7a08e5e6af5e5774a48
Connor Williams <connor.williams@beelivery.com>
2022-12-28 15:31:52 +0000

    init

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="App">
+    <HomePage />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.App {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+
+<style>
+html, body{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ components/HomePage.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+
+const count = ref(1);
+const double = computed(() => count.value * 2);
+let test: string = 'hello';
+test = 1;
+</script>
+
+<template>
+  <div class="HomePage">
+    hello
+    <img class="HomePage__laptop" src="/img/laptop.png" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.HomePage {
+  width: 100%;
+  height: 100%;
+  background: rgb(39, 41, 50);
+  background: linear-gradient(
+    90deg,
+    rgba(39, 41, 50, 1) 0%,
+    rgba(42, 41, 51, 1) 39%,
+    rgba(64, 51, 64, 1) 100%
+  );
+
+  &__laptop {
+    width: 600px;
+  }
+}
+</style>
